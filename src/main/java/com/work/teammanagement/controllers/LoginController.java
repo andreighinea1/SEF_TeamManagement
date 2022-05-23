f41f1209f68f7b15318d0279ec068ac55c0ff94c
Casi <nicolae.fara@student.upt.ro>
2022-05-23 03:54:03 +0300

    STM-10 FXML Update.

--- src/main/java/com/work/teammanagement/controllers/LoginController.java
+++ src/main/java/com/work/teammanagement/controllers/LoginController.java
@@ -1,4 +1,43 @@
 package com.work.teammanagement.controllers;
 
+import com.work.teammanagement.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Button goToRegisterPageButton;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private ChoiceBox <String> roleChoiceBox;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordPasswordField;
+
+    public LoginController() {
+    }
+
+    public void initialize()
+    {
+
+    }
+
+    public void exitApplication() {
+        javafx.application.Platform.exit();
+    }
+
+    public void goToRegisterPage(ActionEvent actionEvent) {
+        Main.changeScene("register-page");
+    }
+
+    public void login(ActionEvent actionEvent) {
+    }
 }
