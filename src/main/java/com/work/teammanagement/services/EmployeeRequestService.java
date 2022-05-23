3e5e32e941ee615c289bdb717fd4b619b19c338b
Andrei GHINEA <andreidezvoltator@gmail.com>
2022-05-23 11:05:54 +0300

    STM-95 Plan holidays service

---
+++ src/main/java/com/work/teammanagement/services/EmployeeRequestService.java
@@ -0,0 +1,17 @@
+package com.work.teammanagement.services;
+
+import com.work.teammanagement.exceptions.ManagerCannotHaveRequestsException;
+import com.work.teammanagement.exceptions.NotManagerException;
+import com.work.teammanagement.exceptions.UserNotFoundException;
+import com.work.teammanagement.exceptions.UserNotLoggedInException;
+import com.work.teammanagement.model.databases.EmployeeRequestsDB;
+
+public final class EmployeeRequestService {
+    public static void planHoliday(String startDate, String endDate, String requestText, String managerUserName) throws UserNotFoundException, ManagerCannotHaveRequestsException, UserNotLoggedInException, NotManagerException {
+        EmployeeRequestsDB.addRequest("Vacation: " + startDate + "-" + endDate, requestText, managerUserName);
+    }
+
+    public static void requestWorkFromHome(String startDate, String endDate, String requestText, String managerUserName) throws UserNotFoundException, ManagerCannotHaveRequestsException, UserNotLoggedInException, NotManagerException {
+        EmployeeRequestsDB.addRequest("Work from home: " + startDate + "-" + endDate, requestText, managerUserName);
+    }
+}
